350be61ca99573d3e8b29db66aee00915fa816d6
Daniel Engvall <daniel.engvall@amelia.com>
2024-10-07 16:52:01 +0200

    Add yearly code count and chart generation
    Introduced functionality to calculate and display lines of code per year across multiple sources. Enhanced repository summary to support markdown, JSON, and rich text output formats, including a markdown table and bar chart showing lines of code.

--- count_my_code.py
+++ count_my_code.py
@@ -1,9 +1,23 @@
+# /// script
+# requires-python = "==3.12"
+# dependencies = [
+#     "millify==0.1.1",
+#     "py-markdown-table==1.1.0",
+#     "pygount==1.8.0",
+#     "requests==2.32.3",
+#     "rich==13.9.1",
+#     "typer==0.12.5",
+# ]
+# ///
+
 import datetime
 import json
 import collections
 import sys
 from json import JSONDecodeError
-from typing import Literal, Tuple, Dict, Any, Annotated
+from typing import Literal, Any, Annotated
+
+from millify import millify
 from pygount.command import pygount_command
 import requests
 from tempfile import NamedTemporaryFile, TemporaryDirectory
@@ -17,6 +31,10 @@ import logging
 from rich.console import Console
 from rich import print_json
 from rich.markdown import Markdown
+import warnings
+
+
+warnings.simplefilter("ignore")
 
 
 logging.basicConfig(level=logging.INFO, datefmt="[%X]", format="%(message)s", handlers=[RichHandler()])
@@ -25,11 +43,14 @@ logger = logging.getLogger(__name__)
 
 EXCLUDE_PATTERN = "...,zope,twisted,garden"
 
-Repository = collections.namedtuple('repo', 'name url year')
-CountSummary = dict[Literal['dirs', 'github'], collections.Counter]
+Repository = collections.namedtuple("repo", "name url year")
+CountSummary = dict[Literal["dirs", "github"], collections.Counter]
 ReposSummary = tuple[dict[str | Any, dict]]
-Years = Annotated[set, 'years']
-SourceLinesPerYear = tuple[Annotated[str, 'source name'], Annotated[collections.defaultdict[str, list], 'source and per year']]
+Years = Annotated[set, "years"]
+SourceLinesPerYear = tuple[
+    Annotated[str, "source name"],
+    Annotated[collections.defaultdict[str, list], "source and per year"],
+]
 
 cli_app = typer.Typer()
 
@@ -39,41 +60,45 @@ class OutputFormat(StrEnum):
     JSON = auto()
     RICH = auto()
 
+
 def get_code_per_year_source(summary: dict) -> tuple[Years, SourceLinesPerYear]:
     all_years = set()
     for source, repos in summary.items():
         for repo, data in repos.items():
-            all_years.add(data['year'])
+            all_years.add(data["year"])
     source_lines_per_year = collections.defaultdict(list)
     for source, repos in summary.items():
         c = collections.Counter()
         for repo, data in repos.items():
-            c[data['year']] += data['totalCodeCount']
+            c[data["year"]] += data["totalCodeCount"]
         for year, count in c.items():
             source_lines_per_year[source].append(count)
     return all_years, source_lines_per_year
 
 
-def code_per_year_to_chart(all_years: Years, per_source: SourceLinesPerYear,
-                           title:str='Line of codes per source',
-                           x_axis_title: str='Year') -> str:
-    prefix = f'''
+def code_per_year_to_chart(
+    all_years: Years,
+    per_source: SourceLinesPerYear,
+    title: str = "Line of codes per year",
+    x_axis_title: str = "Year",
+) -> str:
+    prefix = f"""
 ```mermaid
 xychart-beta
       title "{title}"
       x-axis "{x_axis_title}" {json.dumps(list(all_years))}
-'''
+"""
     data_lines = []
     for source, data_list in per_source.items():
         data_lines.append(f'      bar "{source.split('/').pop()}" {json.dumps(data_list)}')
-    return prefix + '\n'.join(data_lines) + '```'
+    return prefix + "\n".join(data_lines) + "```"
 
 
-def get_all_github_repos(user='engdan77') -> list[Repository]:
+def get_all_github_repos(user="engdan77") -> list[Repository]:
     repos = []
-    for r in requests.get(f'https://api.github.com/users/{user}/repos').json():
-        year = r['created_at'].split('-').pop(0)
-        repos.append(Repository(r['name'], f"{r['html_url']}.git", year))
+    for r in requests.get(f"https://api.github.com/users/{user}/repos").json():
+        year = r["created_at"].split("-").pop(0)
+        repos.append(Repository(r["name"], f"{r['html_url']}.git", year))
     return sorted(repos, key=lambda x: x.year)
 
 
@@ -83,14 +108,14 @@ def get_repo_summary_file(tmp_file: NamedTemporaryFile, repo_folder: Path | str)
         f = repo_folder.as_posix()
     else:
         f = repo_folder.url
-    args = ['--folders-to-skip', EXCLUDE_PATTERN] if EXCLUDE_PATTERN else []
-    args.extend(['--format', 'json', '--out', tmp_file.name, f])
+    args = ["--folders-to-skip", EXCLUDE_PATTERN] if EXCLUDE_PATTERN else []
+    args.extend(["--format", "json", "--out", tmp_file.name, f])
     pygount_command(args)
     try:
-        summary = json.loads(open(tmp_file.name).read())['summary']
-        summary['year'] = get_repo_create_year(repo_folder)
+        summary = json.loads(open(tmp_file.name).read())["summary"]
+        summary["year"] = get_repo_create_year(repo_folder)
     except JSONDecodeError:
-        logger.warning(f'Unable to find data for {repo_folder}')
+        logger.warning(f"Unable to find data for {repo_folder}")
         summary = {}
     return summary
 
@@ -106,38 +131,42 @@ def get_repo_create_year(source: str | Repository) -> int:
         return year
 
 
-
-
 def get_summaries(sources, parse_sub_folders_as_repos) -> dict[str, dict]:
     summarization = collections.defaultdict(dict)
     with NamedTemporaryFile() as tmp_file:
-        logger.info(f'Temp file {tmp_file.name}')
+        logger.info(f"Temp file {tmp_file.name}")
         for source in sources:
             repo_item: Path | str = Path(source)
             if repo_item.is_dir():
                 if parse_sub_folders_as_repos:
-                    process_sources = repo_item.glob('*/')
+                    process_sources = repo_item.glob("*/")
                 else:
                     process_sources = [repo_item]
             else:
                 process_sources = get_all_github_repos(repo_item)
 
             for process_source in process_sources:
-                logger.info(f'Processing {source} / {process_source.name.strip()}')
+                logger.info(f"Processing {source} / {process_source.name.strip()}")
                 summary = get_repo_summary_file(tmp_file, process_source)
                 summarization[source][process_source.name] = summary
     return summarization
 
 
 def output_as_markdown(summaries: dict[str, dict]) -> str:
-    output_markdown = ''
+    output_markdown = ""
 
     for source in summaries:
         table_data = []
-        source_data = dict(sorted(summaries[source].items(), key=lambda x: x[1]['year']))
+        source_data = dict(sorted(summaries[source].items(), key=lambda x: x[1]["year"]))
         output_markdown += f'### {source.split('/').pop()}\n\n'
         for repo_name, data in source_data.items():
-            table_data.append({'repo_name': repo_name, 'year': data['year'], 'lines_of_code': data['totalCodeCount']})
+            table_data.append(
+                {
+                    "repo_name": repo_name,
+                    "year": data["year"],
+                    "lines_of_code": data["totalCodeCount"],
+                }
+            )
         output_markdown = markdown_table(table_data).get_markdown()
 
     all_years, data = get_code_per_year_source(summaries)
@@ -149,24 +178,40 @@ def output_as_markdown(summaries: dict[str, dict]) -> str:
 def output_as_json(summaries: dict[str, dict]) -> dict:
     output_json = {}
     for source in summaries:
-        source_data = dict(sorted(summaries[source].items(), key=lambda x: x[1]['year']))
+        source_data = dict(sorted(summaries[source].items(), key=lambda x: x[1]["year"]))
         source_name = f'{source.split('/').pop()}'
+        repos_result = []
         for repo in source_data:
-            output_json[source] = []
-            output_json[source].append({'repo_name': repo, 'year': source_data['year'], 'lines_of_code': source_data['totalCodeCount']})
-
-    all_years, data = get_code_per_year_source(summaries)
-    output_json['per_years'] = all_years
+            repos_result.append(
+                {
+                    "repo_name": repo,
+                    "year": source_data[repo]["year"],
+                    "lines_of_code": source_data[repo]["totalCodeCount"],
+                }
+            )
+        output_json[source_name] = repos_result
     return output_json
 
 
+def get_summary_of_all(summaries: dict[str, dict]) -> dict[str, int]:
+    years = set()
+    total_count = 0
+    repo_count = 0
+    for source, repos in summaries.items():
+        for repo_name, repo_data in repos.items():
+            repo_count += 1
+            years.add(repo_data["year"])
+            total_count += repo_data["totalCodeCount"]
+    across_years = max(years) - min(years)
+    return {'total_lines': total_count, 'repo_count': repo_count, 'across_years': across_years}
+
 
 @cli_app.command()
-def repos_summary(repos: list[str] = typer.Argument(..., help='Either a path or an URL'),
-                  sub_folders_as_repos: bool = typer.Option(True, help='Treat sub-folders as repositories'),
-                  output_format: OutputFormat = typer.Option(OutputFormat.RICH, help='Output format')) \
-        -> tuple[ReposSummary, CountSummary]:
-
+def repos_summary(
+    repos: list[str] = typer.Argument(..., help="Either a path or an URL"),
+    sub_folders_as_repos: bool = typer.Option(True, help="Treat sub-folders as repositories"),
+    output_format: OutputFormat = typer.Option(OutputFormat.RICH, help="Output format"),
+) -> tuple[ReposSummary, CountSummary]:
     r = get_summaries(repos, sub_folders_as_repos)
     match output_format:
         case OutputFormat.MARKDOWN:
@@ -175,16 +220,15 @@ def repos_summary(repos: list[str] = typer.Argument(..., help='Either a path or 
             console = Console()
             md = Markdown(output_as_markdown(r))
             console.print(md)
+            d = get_summary_of_all(r)
+            console.print(f'[bold]Total line codes: [/bold] {millify(d['total_lines'])}')
+            console.print(f'[bold]Projects: [/bold] {d['repo_count']}')
+            console.print(f'[bold]Across years: [/bold] {d['across_years']}')
         case OutputFormat.JSON:
-            print_json(json.dumps(output_as_json(r)))
-
-
-
-
-
-
-
+            d = output_as_json(r)
+            print_json(json.dumps(d))
 
 
 if __name__ == "__main__":
     cli_app()
+
